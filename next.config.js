f95fdfea0d05d234ccb6e8c865761a1758211a02
MOMOH ABDUL JABBAR <momohabduljabbar@gmail.com>
2023-04-21 13:27:17 -0700

    yes commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  experimental: {
+    images: true,
+  },
 };
 
 module.exports = nextConfig;
